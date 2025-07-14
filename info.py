43768c85de96168a01e569bb95030ac26cb01f8d
somu974323 <somu974323@gmail.com>
2025-07-14 12:48:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,8 +61,8 @@ OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'deendayaldhaked@ybl')
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://vinod974323:W2eL86Tb02WsWrEH@cluster0.d9rx0bu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://vinod974323:KbG1aVFXp5JnlESx@cluster0.petmpxp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://vinod974323:xjt3vj3s3AVekAx4@cluster0.aatft4o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://vinod974323:FavSjqez5PgI48Yg@cluster0.zxqcqam.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cluster")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'SK_MOVIESZ')
 
