09b6a4a8c5d361f7aa9a43015f0e3f2e36b9d400
somu974323 <somu974323@gmail.com>
2025-10-07 14:12:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -87,7 +87,7 @@ HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/dreamcinezone')  # Ho
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'lksfy.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'd200e6d92af0bf242b9d25fce8c9b7023bb7720e')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Link_08/30')  # Tutorial video link for opening shortlink website
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Link_06')  # Tutorial video link for opening shortlink website
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 
 # ============================
