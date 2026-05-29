8ae61999c752cb27fb79576696e72a42f87d2e73
somu974323 <somu974323@gmail.com>
2026-05-29 23:36:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -85,8 +85,8 @@ HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/How_To_Open_Link_06')
 # Link Shortener Configuration
 # ============================
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'linkshortify.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'd200e6d92af0bf242b9d25fce8c9b7023bb7720e')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'vplink.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'b7751d50c1506a1731d5e863a584a294657a55c8')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Link_06')  # Tutorial video link for opening shortlink website
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 
